fbdbc5091e02e04bbcb98f87c5ec8763251ed4bc
RBeta <rbnbet@gmail.com>
2013-07-16 13:03:26 -0500

    Create base rails app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+StarfishApp::Application.config.secret_key_base = 'ba8422ee00f4c169624971f89549e9085048b83f9829f3485bd0f28c6dc4557a215057dacf15e941fe50822655acbe9c0e993014ca5083ab182b90ca09947a3b'
