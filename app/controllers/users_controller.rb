51cb280ab087fbf2235c6afbafcfa5696d6c535e
Edgar <edgar@edgar-laptop.(none)>
2013-07-22 16:47:55 -0500

    Administrator model

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,7 +3,9 @@ class UsersController < ApplicationController
   before_filter :authenticate_user!, except:[:index,:show]
 
   def index
-    #
+    if current_user.try(:admin?)
+      redirect_to(administrators_index_path)
+    end
   end
 
   def show
