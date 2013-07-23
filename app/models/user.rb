d01ae17f4cc55999a83576f9ca5d9d883388c787
RBeta <rbnbet@gmail.com>
2013-07-23 15:15:30 -0500

    Create the Backs model and change the category id type

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,6 @@
 class User < ActiveRecord::Base
   has_many :projects
+  has_many :backs
   # Include default devise modules. Others available are:
   # :token_authenticatable, :confirmable,
   # :lockable, :timeoutable and :omniauthable
