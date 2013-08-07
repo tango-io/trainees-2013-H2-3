38d915909b6879b39aff695c1136f7dcf0ce7d83
Stiffen Alejandro Macias Quintero <stiffen.macias@tangosource.com>
2013-08-07 12:06:59 -0500

    Conditions in method project_time_to_close changed

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -4,6 +4,7 @@ class ProjectsController < ApplicationController
 
   def index
     @projects = Project.all
+
     respond_to do |format|
       format.html
       format.json { render json: @projects}
@@ -11,15 +12,24 @@ class ProjectsController < ApplicationController
   end
 
   def show
+    flash[:notice] = ""
     @project = Project.find(params[:id])
     @users = User.joins(:backs).where(backs: {project_id: params[:id]})
     @owner = User.find(@project.user_id)
+    project_time_to_close
     respond_to do |format|
       format.html
     end
   end
   def project_time_to_close
-      
+    @project = Project.find(params[:id])
+    @time_now = Time.now
+    if @time_now >= @project.close_date
+      flash[:notice] = "The Project was Closer"
+      @close_date = 1
+    else
+      @close_date = 0
+    end
   end
 private
 
