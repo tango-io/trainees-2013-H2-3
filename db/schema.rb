919067fd78db2f53db3c1b860aee4c41677efd68
oscardev <oscardev@oscardev-AOD270.(none)>
2013-08-13 11:40:14 -0500

    change sign and logout view to center of the site

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20130731195304) do
+ActiveRecord::Schema.define(version: 20130726032434) do
 
   create_table "backs", force: true do |t|
     t.integer  "amount"
