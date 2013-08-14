453226e5062881af381fd4f128d0eb90947d7e60
Stiffen Alejandro Macias Quintero <stiffen.macias@tangosource.com>
2013-08-14 10:46:19 -0500

    Commented method revenue and added method monney_to_give_bweten_dates

--- app/controllers/admin/backs_controller.rb
+++ app/controllers/admin/backs_controller.rb
@@ -8,34 +8,36 @@ class Admin::BacksController < Admin::BaseController
     @back = Back.find(params[:id])
   end
 
-  def revenue
+  #def revenue
 
-    @backs = Back.all
-    @start_date = params[:s_date].to_time
-    @end_date = params[:e_date].to_time
-    @s_int = @start_date.to_i
-    @e_int = @end_date.to_i
-    @time_backs = []
-    @fee = 0.05
-    @sum = 0
-    @int_array = []
+    #@backs = Back.all
+    #@start_date = params[:s_date].to_time
+    #@end_date = params[:e_date].to_time
+    #@s_int = @start_date.to_i
+    #@e_int = @end_date.to_i
+    #@time_backs = []
+    #@fee = 0.05
+    #@sum = 0
+    #@int_array = []
     
-    @backs.each do |back|
-      integer = back.created_at.to_i
-      if (integer > @s_int and integer < @e_int)
-        @time_backs[back.id] = back
-      end
-      @int_array[back.id] = integer 
-    end
-    @i = 0
-    @time_backs.each do |tback|
-      @sum += tback.amount if tback
-      @i += 1 if tback
-    end
+    #@backs.each do |back|
+      #integer = back.created_at.to_i
+      #if (integer > @s_int and integer < @e_int)
+        #@time_backs[back.id] = back
+      #end
+      #@int_array[back.id] = integer 
+    #end
+    #@i = 0
+    #@time_backs.each do |tback|
+      #@sum += tback.amount if tback
+      #@i += 1 if tback
+    #end
 
-    @total_fee = @sum * @fee
-    @backs = @time_backs
-    calculate_backs
+    #@total_fee = @sum * @fee
+    #@backs = @time_backs
+    #calculate_backs
+  #end
+  def monney_to_give_betwen_dates
   end
 
   def monney_to_give
@@ -60,10 +62,7 @@ class Admin::BacksController < Admin::BaseController
     @new_backs.each{|key,sum| @sum += sum }
     @total_fee = @sum * @fee
     @money = @sum - @total_fee
-    #end
-
   end
- 
   
   private
   def back_params
