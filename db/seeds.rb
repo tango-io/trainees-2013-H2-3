c3b6d066246e9ec084359cb2e27329a5e107317d
Stiffen Alejandro Macias Quintero <stiffen.macias@tangosource.com>
2013-08-15 16:15:23 -0500

    Fixing error in db/seeds.rb on lines 32 to 35

--- db/seeds.rb
+++ db/seeds.rb
@@ -29,9 +29,9 @@ city[4] = "DF"
 description = "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
 #Create Project
 for x in (0..4) 
-  Project.create(user_id: john.id ,name: "Oscar Project #{x}", city: city[x], close_date: Time.now + x, description: description, amount: x*1000, money_raised: x*100, video_url: File.open(File.join(Rails.root, "/db/fixtures/videos/out-#{x}.ogv")),category_id: category[x].id, approved: true)
-  Project.create(user_id: mike.id ,name: "Mike Project #{x}", city: city[x], close_date: Time.now + x, description: description, amount: x*1000, money_raised: x*100, video_url: File.open(File.join(Rails.root, "/db/fixtures/videos/out-#{x}.ogv")),category_id: category[x].id, approved: true)
-  Project.create(user_id: stial.id ,name: "Stial Project #{x}", city: city[x], close_date: Time.now + x, description: description, amount: x*1000, money_raised: x*100, video_url: File.open(File.join(Rails.root, "/db/fixtures/videos/out-#{x}.ogv")),category_id: category[x].id, approved: true)
+  Project.create(user_id: john.id ,name: "Oscar Project #{x}", city: city[x], close_date: Time.now + x.days, description: description, amount: x*1000, money_raised: x*100, video_url: File.open(File.join(Rails.root, "/db/fixtures/videos/out-#{x}.ogv")),category_id: category[x].id, approved: true)
+  Project.create(user_id: mike.id ,name: "Mike Project #{x}", city: city[x], close_date: Time.now + x.days, description: description, amount: x*1000, money_raised: x*100, video_url: File.open(File.join(Rails.root, "/db/fixtures/videos/out-#{x}.ogv")),category_id: category[x].id, approved: true)
+  Project.create(user_id: stial.id ,name: "Stial Project #{x}", city: city[x], close_date: Time.now + x.days, description: description, amount: x*1000, money_raised: x*100, video_url: File.open(File.join(Rails.root, "/db/fixtures/videos/out-#{x}.ogv")),category_id: category[x].id, approved: true)
 end
 
 
