e7bff52200de157476df6b20c08b6e8bdbe366bf
Stiffen Alejandro Macias Quintero <stiffen.macias@tangosource.com>
2013-08-27 10:34:34 -0500

    routes changed

--- config/routes.rb
+++ config/routes.rb
@@ -6,11 +6,19 @@ StarfishApp::Application.routes.draw do
   root 'home#index'
   #Users page
   devise_for :users, controller: 'user/users', path_names: {sing_in: "login", sing_out: "logout"}
-  namespace :user do
-    resources :projects
+  resources :projects do
+    resources :backs do 
+      get :list 
+    end
   end
-  resources :backs, controller: 'user/backs'
-  get 'users/backs', to: 'user/backs#list' 
+  resources :users do
+    resources :projects
+    resources :backs do 
+      get :list 
+    end
+  end
+  #resources :backs, controller: 'user/backs
+  #get 'users/backs', to: 'user/backs#list' 
   #admin page
   namespace :admin do
     resources :categories, :home 
