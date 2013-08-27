a962e2e0a2b63a1eb95bb7bd8f6a1df7fd36d860
Stiffen Alejandro Macias Quintero <stiffen.macias@tangosource.com>
2013-08-27 12:26:32 -0500

    added colums in migration

--- db/migrate/20130827171417_add_fields_pledges.rb
+++ db/migrate/20130827171417_add_fields_pledges.rb
@@ -1,4 +1,7 @@
 class AddFieldsPledges < ActiveRecord::Migration
   def change
+    add_column :pledges, :min, :float
+    add_column :pledges, :max, :float
+    add_column :pledges, :description
   end
 end
