8af2adf6d2d74c107c2e1f7b75a3b617ad8e2506
Stiffen Alejandro Macias Quintero <stiffen.macias@tangosource.com>
2013-08-28 12:10:52 -0500

    Changed Form in pledge new and added method create and params in pladges_controller

--- app/controllers/user/pledges_controller.rb
+++ app/controllers/user/pledges_controller.rb
@@ -1,6 +1,12 @@
 class User::PledgesController < User::BaseController
  before_filter :authenticate_user!
   def new
-    @plegde = Pledge.new
+    @pledge = Pledge.new
+    @user = User.new
+  end
+  def create
+  end
+  def params
+
   end
 end
