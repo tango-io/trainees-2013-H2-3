8c1abe9a931adf055c5fb386cb5bddc3ea062d6f
Stiffen Alejandro Macias Quintero <stiffen.macias@tangosource.com>
2013-09-02 11:53:15 -0500

    Some changes

--- app/assets/javascripts/angular/controllers/backsController.js
+++ app/assets/javascripts/angular/controllers/backsController.js
@@ -1,14 +1,16 @@
 starFishModel.controller('backsController', ['$scope','$http', function($scope, $http){
   $scope.backs = {};
 
-  $scope.validateAmount = function(){
-    pledge_selected = $("h5").html();
+  $scope.validateAmount = function(pledge_id){
+    pledge_selected = $("h5[data-id = '"+ pledge_id +"']").html();
     amount = parseInt(($("input[name='back[amount]']")).val());
-    
-    if (pledge_selected < amount) {
       
+    if (pledge_selected <= amount) {
+      console.log("BUENO = "+pledge_selected +" - "+amount+"'");
+      return false;
     }else{
-    
+      console.log("MALO = "+pledge_selected+" - "+amount+"");
+      return true;
     }
   };
 }]);
